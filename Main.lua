768a385e6860279b7f134cdedf208bf9b05ba8fa
Metacalled <73370902+Metacalled2@users.noreply.github.com>
2020-12-20 19:25:41 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,15 +3,39 @@ local HttpSer = game:GetService'HttpService'
 
 local Admins = HttpSer:JSONDecode(game:HttpGet"https://raw.githubusercontent.com/Metacalled2/DIR/main/Hosts/AdminTable")
 
+for i = 1,#Admins do
+ warn(Admins[i].U)
+end
+
 local function Connect(Obj)
  
  Obj.Chatted:Connect(function(Chat)
    if Chat:lower():sub(1,7) == ("$bring ") then
-     if string.sub(Chat, 8):lower():match(Players.LocalPlayer.Name:lower()) then
+     if Players.LocalPlayer.Name:lower():match(string.sub(Chat, 8):lower()) then
        Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Obj.Character.HumanoidRootPart.CFrame
      end
    end
  end)
+ 
+  Obj.Chatted:Connect(function(Chat)
+   if Chat:lower():sub(1,6) == ("$kill ") or Chat:lower():sub(1,9) == ("/e $kill ") then
+    
+     if Chat:lower():sub(1,6) == ("$kill ") then
+        if Players.LocalPlayer.Name:lower():match(string.sub(Chat, 7):lower()) then
+          Players.LocalPlayer.Character:BreakJoints()
+        end
+     end
+    
+     if Chat:lower():sub(1,9) == ("/e $kill ") then
+        if Players.LocalPlayer.Name:lower():match(string.sub(Chat, 10):lower()) then
+          Players.LocalPlayer.Character:BreakJoints()
+        end
+     end
+    
+   end
+ end)
+
+end
 
 local function IsAdmin(ID)
   
@@ -36,4 +60,3 @@ Players.PlayerAdded:Connect(function(...)
     Connect(Player[1])
    end
 end)
-
